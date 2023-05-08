53c2506319636ccb15d562324be0b1de93b0f5b2
Sergejs-Rudko <sergejs.rudko@gmail.com>
2023-05-09 00:33:18 +0100

    feat: 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,53 +1,49 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
 const distPath = path.join(__dirname, 'project-dist');
-let content = [];
 
-const beforeEach = async () => {
+(async function mergeStyles() {
   try {
-    const distFolderFileList = await fs.readdir(distPath);
-    if (!distFolderFileList.includes('bundle.css')) {
-      createBundle().then(() => {
-        console.log('created for a first time');
-      });
-    } else {
-      fs.unlink(bundlePath).then(() => {
+    const distFolderFileList = await fs.promises.readdir(distPath);
+    if (distFolderFileList.includes('bundle.css')) {
+      fs.promises.unlink(bundlePath).then(() => {
         console.log('deleted');
       });
-      createBundle().then(
-        () => {
-          console.log('created again');
-        }
-      );
-
     }
-  } catch (e) {
+    filesAndFolders('styles').then((links) => {
+      links = links.map((l) => path.join(__dirname,'styles',l));
+      combineFiles(links).then((text) => {
+        fs.promises.writeFile(bundlePath,text);
+      });
+    });
+  } catch (err) {
     console.log('file does not exist');
   }
-};
+})();
 
-const createBundle = async () => {
-  fs.writeFile(bundlePath, content).then(() => {
-    console.log('created');
-  });
-};
-
-const filesAndFolders = async (folder, content) => {
+const combineFiles = async (sources) => {
+  let text = '';
   try {
-    const list = await fs.readdir(folder, {withFileTypes: true});
-    const files = list.filter((el) => el.isFile() && el.name.endsWith('.css'));
-    const folders = list.filter((el) => el.isDirectory());
-    console.log(files);
-    files.forEach(file => content.push(`${file.name}`));
-    return content;
-  } catch (e) {
-    console.log(e);
+    const filePromises = sources.map((source) => fs.promises.readFile(source, 'utf-8'));
+    const fileContents = await Promise.all(filePromises);
+    text = fileContents.join('');
+    return text;
+  } catch (err) {
+    console.log(err);
   }
 };
 
-filesAndFolders(__dirname).then(() => {
-  console.log('process finished');
-  console.log(content);
-});
-
+const filesAndFolders = async (folder) => {
+  const folderPath = path.join(__dirname, folder);
+  try {
+    const list = await fs.promises.readdir(folderPath, {withFileTypes: true});
+    const files = list.filter((el) => el.isFile() && el.name.endsWith('.css'));
+    return files.map((f) => {
+      const file = f.name;
+      return file;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
